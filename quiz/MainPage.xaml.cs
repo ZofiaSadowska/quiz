b0cf1855ac754bdade79f4fa1fd0d9692fb21f67
ZofiaSadowska <zofiasadowska696@gmail.com>
2025-11-24 08:24:44 +0100

    Reapply "tesrt"
    This reverts commit de991a35edab3c681de8794694c051ebbdb9f3bc.

---
+++ quiz/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace quiz
+{
+    public partial class MainPage : ContentPage
+    {
+        private string filePath = Path.Combine(FileSystem.AppDataDirectory, "pytnia.json");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+       
+    }
+}
